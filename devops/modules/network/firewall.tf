1d041b3be7e507655257e53df3fde316a0abbaa3
Alan Fang <disflyer123@gmail.com>
2025-03-15 00:48:31 +0800

    feat: update

--- devops/modules/network/firewall.tf
+++ devops/modules/network/firewall.tf
@@ -19,7 +19,7 @@ resource "google_compute_firewall" "default_ingress" {
   target_tags   = ["internal"]
 }
 
-# 默认出口规则
+# 默认出口规则 - 阻止所有流量
 resource "google_compute_firewall" "default_egress" {
   name      = "${var.env}-deny-external-egress"
   network   = google_compute_network.vpc.name
@@ -31,22 +31,28 @@ resource "google_compute_firewall" "default_egress" {
 
   destination_ranges = ["0.0.0.0/0"]
   priority           = 1000
+  target_tags        = ["restricted-egress"]
+}
 
-  # 例外允许NTP和DNS
+# 例外允许规则 - 允许NTP和DNS
+resource "google_compute_firewall" "allow_essential" {
+  name      = "${var.env}-allow-essential-egress"
+  network   = google_compute_network.vpc.name
+  direction = "EGRESS"
+
   allow {
     protocol = "udp"
-    ports    = ["123"]
+    ports    = ["123", "53"]  # NTP和DNS
   }
+  
   allow {
     protocol = "tcp"
-    ports    = ["53"]
-  }
-  allow {
-    protocol = "udp"
-    ports    = ["53"]
+    ports    = ["53"]  # DNS
   }
 
-  target_tags = ["restricted-egress"]
+  destination_ranges = ["0.0.0.0/0"]
+  priority           = 900  # 优先级高于deny规则
+  target_tags        = ["restricted-egress"]
 }
 
 # 自定义规则
