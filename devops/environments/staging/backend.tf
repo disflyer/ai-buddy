a5476e660423369e8cb56166624d73e1c179db5a
Alan Fang <disflyer123@gmail.com>
2025-03-15 17:32:31 +0800

    feat: update

---
+++ devops/environments/staging/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "gcs" {
+    bucket  = "tfstate-rare-attic"            # 存储桶名称
+    prefix  = "terraform/staging"             # 状态文件路径前缀
+    credentials = "../../service-account.json" # 服务账号凭证路径
+  }
+} 
